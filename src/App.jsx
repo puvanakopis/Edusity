147646789f7bda8fa9e3e71707bce730a3dbc330
puvanakopis <puvanakopis@gmail.com>
2025-03-02 16:20:54 +0530

    Home and Programs component are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Component/Navbar/Navbar'
 import Hero from './Component/Hero/Hero'
+import Program from './Component/Program/Program'
+import Title from './Component/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Title title="Our Program" subTitle="What We Offer"/>
+      <Program/>
     </div>
   )
 }
